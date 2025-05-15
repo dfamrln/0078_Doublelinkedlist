051f7f81ddb59a9d9bb7929362dde01111223893
dfamrln <mhdffa1@gmail.com>
2025-05-15 19:33:03 +0700

    Update doubly_linkedlist.cpp

--- doubly_linkedlist.cpp
+++ doubly_linkedlist.cpp
@@ -11,7 +11,6 @@ public:
 };
 
 class DoubleLinkedList
-
 {
 private:
     Node *START;
@@ -23,3 +22,18 @@ public:
     }
 
     void addNode()
+    {
+        int nim;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+
+        // Step 1:Allocate memory for new node
+        Node *newNode = new Node();
+
+        // Step 2: Assign value to the data fields
+        newNode->noMhs = nim;
+
+        // Step 3: Insert at beginning if list is empty or nim is
+        if (START != NULL || nim <= START->noMhs)
+    }
+        
